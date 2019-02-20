eafcc0ff9d757295c57c1b36e2e06dba05221194
Kashif Memon <kashif.memon@risksense.com>
2019-02-20 16:42:53 -0700

    + incomplete-4

---
+++ solutions-to-leetcode/4_median_of_two_sorted_arrays.py
@@ -0,0 +1,31 @@
+"""
+4. Median of Two Sorted Arrays
+https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+def findMedianSortedArrays(nums1: 'List[int]', nums2: 'List[int]') -> 'float':
+    sorted_joined = sorted(nums1 + nums2)
+    if len(sorted_joined) % 2 == 0:
+        print (len(sorted_joined)/2)
+        # print (sorted_joined[int(len(sorted_joined)/2)])
+        # return sorted_joined[(int((len(sorted_joined) + 1) / ))]
+    else:
+        return sorted_joined[(int((len(sorted_joined) + 1) / 2))]
+
+
+def main():
+    nums11 = [1, 3]
+    nums12 = [2]
+    # print("The median is", findMedianSortedArrays(nums11, nums12))
+
+    nums21 = [1, 2]
+    nums22 = [3, 4]
+    print("The median is", findMedianSortedArrays(nums21, nums22))
+
+
+if __name__ == "__main__":
+    main()
