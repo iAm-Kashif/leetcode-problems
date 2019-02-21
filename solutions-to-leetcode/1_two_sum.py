69d6b28e2a8162a56d3f856f7653056a78215c0c
Kashif Memon <kashif.memon@risksense.com>
2019-02-21 13:36:29 -0700

    + 1 + incomplete-942

---
+++ solutions-to-leetcode/1_two_sum.py
@@ -0,0 +1,38 @@
+"""
+1. Two Sum
+https://leetcode.com/problems/two-sum/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+def twoSum(nums: 'List[int]', target: 'int') -> 'List[int]':
+    dict = {}
+    for i in range(0, len(nums)):
+        if nums[i] in dict:
+            return [dict[nums[i]], i]
+    else:
+        dict[target - nums[i]] = i
+    """
+    for index in range(len(nums)):
+        if (target - nums[index]) in nums:
+            if index == nums.index(target-nums[index]):
+                continue
+            else:
+                return [index, nums.index(target-nums[index])]
+    """
+
+
+def main():
+    nums1 = [2, 7, 11, 15]
+    target1 = 9
+    # print(twoSum(nums1, target1))
+
+    nums2 = [3, 2, 3]
+    target2 = 6
+    print(twoSum(nums2, target2))
+
+
+if __name__ == "__main__":
+    main()
---
+++ solutions-to-leetcode/942_di_string_match.py
@@ -0,0 +1,34 @@
+"""
+942. DI String Match
+https://leetcode.com/problems/di-string-match/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+def diStringMatch(S: 'str') -> 'List[int]':
+    out = []
+    val = 1
+    for character in S:
+        if character == "I":
+            val += 1
+        else:
+            val -= 1
+        out.append(val)
+    return out
+
+
+def main():
+    input1 = "IDID"
+    print(diStringMatch(input1))
+
+    input2 = "III"
+    print(diStringMatch(input2))
+
+    input3 = "D"
+    print(diStringMatch(input3))
+
+
+if __name__ == "__main__":
+    main()
