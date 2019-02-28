971f4dc2caab9173b0ac3a66258e753f130cb296
Kashif Memon <iAmKashifMemon@gmail.com>
2019-02-27 20:16:07 -0700

    + 760

---
+++ solutions-to-leetcode/760_find_anagram_mappings.py
@@ -0,0 +1,24 @@
+"""
+1. Two Sum
+https://leetcode.com/problems/two-sum/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+def anagramMappings(A: 'List[int]', B: 'List[int]') -> 'List[int]':
+    out = []
+    for item in A:
+        out.append(B.index(item))
+    return out
+
+
+def main():
+    A = [12, 28, 46, 32, 50]
+    B = [50, 12, 32, 46, 28]
+    print(anagramMappings(A, B))
+
+
+if __name__ == "__main__":
+    main()
