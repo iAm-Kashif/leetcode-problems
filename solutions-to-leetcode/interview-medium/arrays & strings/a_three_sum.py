ad3116697dbe11a1de8b49baea75efdbe4a7c854
Kashif Memon <kashif.memon@risksense.com>
2019-03-11 12:50:43 -0600

    + others

---
+++ solutions-to-leetcode/interview-medium/arrays & strings/a_three_sum.py
@@ -0,0 +1,20 @@
+"""
+Three Sum
+https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/776/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+class Solution:
+    def threeSum(self, nums: 'List[int]') -> 'List[List[int]]':
+        pass
+
+
+def main():
+    print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
+
+
+if __name__ == "__main__":
+    main()
