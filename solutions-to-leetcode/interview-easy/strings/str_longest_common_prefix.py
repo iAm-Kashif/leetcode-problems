59123995bc10d3cbcb7cd1fef8afae64f8745d20
Kashif Memon <iAmKashifMemon@gmail.com>
2019-03-10 20:32:09 -0600

    + arrays + str

---
+++ solutions-to-leetcode/interview-easy/strings/str_longest_common_prefix.py
@@ -0,0 +1,22 @@
+"""
+Longest Common Prefix
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/887/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: 'List[str]') -> str:
+        
+
+
+def main():
+    print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
+
+    print(Solution().longestCommonPrefix(["dog", "racecar", "car"]))
+
+
+if __name__ == "__main__":
+    main()
--- solutions-to-leetcode/interview-easy/strings/str_valid_palindrome.py
+++ solutions-to-leetcode/interview-easy/strings/str_valid_palindrome.py
@@ -9,7 +9,19 @@ _python_version:    3.7.2
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        pass
+        import re
+        s = re.sub(r"\W","",s).lower()
+
+        for idx in range(len(s)//2):
+            if not s[idx] == s[-(idx+1)]:
+                return False
+        return True
+
+        # OR
+        # s = re.sub(r"\W+", "", s).lower()
+        # return s == s[::-1]
+
+
 
 
 def main():
