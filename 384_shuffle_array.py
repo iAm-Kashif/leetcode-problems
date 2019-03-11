c9273865e07770a55318b20b8dab180e58c93d31
Kashif Memon <kashif.memon@risksense.com>
2019-03-11 11:51:46 -0600

    + design

---
+++ 384_shuffle_array.py
@@ -0,0 +1,27 @@
+"""
+384. Shuffle Array
+https://leetcode.com/problems/shuffle-an-array/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+class Solution(object):
+
+    def __init__(self, nums: List[int]):
+        self._array = nums
+
+    def reset(self) -> 'List[int]':
+        return self._array
+
+    def shuffle(self) -> 'List[int]':
+        import random
+        shuffled_array = self._array[:]
+        random.shuffle(shuffled_array)
+        return shuffled_array
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
