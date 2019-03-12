5c608482b15daf881b6d6eba333f5a3a9be8fbd2
Kashif Memon <kashif.memon@risksense.com>
2019-03-12 12:33:04 -0600

    + linked-list

---
+++ solutions-to-leetcode/206_reverse_linked_list.py
@@ -0,0 +1,36 @@
+"""
+206. Reverse Linked List
+https://leetcode.com/problems/reverse-linked-list/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return []
+        before, current = None, head
+        while current is not None:
+            after = current.next
+            current.next = before
+            before = current
+            current = after
+        return before
+
+
+def main():
+    input1 = ListNode(1)
+    input1.next = ListNode(2)
+    out = Solution().reverseList(input1)
+    print(out.val, out.next.val)
+
+
+if __name__ == "__main__":
+    main()
