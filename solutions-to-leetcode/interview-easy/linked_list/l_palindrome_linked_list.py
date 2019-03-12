cd74eec1818f61ca0fc9f1077815437ec4c09948
Kashif Memon <kashif.memon@risksense.com>
2019-03-12 13:22:37 -0600

    + linked-list

--- solutions-to-leetcode/interview-easy/linked_list/l_palindrome_linked_list.py
+++ solutions-to-leetcode/interview-easy/linked_list/l_palindrome_linked_list.py
@@ -14,7 +14,33 @@ class ListNode:
 
 
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
+    def isPalindrome_1(self, head: ListNode) -> bool:
+        # Method 1 - Stack
+        # Iterate over list, add to Stack
+        # Iterate again and check pop() stack contents
+        # True if all match ,else False
+        if head is None: return True
+        curr = head
+        stack = []
+        while curr is not None:
+            print(">> Adding to Stack", curr.val)
+            stack.append(curr)
+            curr = curr.next
+
+        while head is not None and not len(stack) == 0:
+            if not head.val == (stack.pop()).val:
+                return False
+            head = head.next
+        return True
+
+        # Method 2 - Reverse List
+        # Figure middle of list
+        # Reverse from middle of list
+        # verify if new reverse and middle is same?
+
+    def isPalindrome_2(self, head: ListNode) -> bool:
+        # Method 3 - Recursive
+        
         pass
 
 
@@ -22,7 +48,7 @@ def main():
     input1 = ListNode(1)
     input1.next = ListNode(2)
     input1.next.next = ListNode(2)
-    input1.next.next.next = ListNode(1)
+    input1.next.next.next = ListNode(0)
     print(Solution().isPalindrome(input1))
 
 
