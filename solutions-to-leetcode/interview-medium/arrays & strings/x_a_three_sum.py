6e4f18b21c858c0d2ecd59d9a8c52559114ca1d8
Kashif Memon <iAmKashifMemon@gmail.com>
2019-03-24 11:29:16 -0600

    + leetcode

--- solutions-to-leetcode/interview-medium/arrays & strings/x_a_three_sum.py
+++ solutions-to-leetcode/interview-medium/arrays & strings/x_a_three_sum.py
@@ -9,10 +9,29 @@ _python_version:    3.7.2
 
 class Solution:
     def threeSum(self, nums: 'List[int]') -> 'List[List[int]]':
-        if len(nums) < 3: return False
-        from itertools import permutations
-        sol = dict.fromkeys(tuple(sorted(i)) for i in permutations(nums, 3) if sum(i) == 0)
-        return sol.keys()
+        out = []
+        nums.sort()
+
+        for idx in range(len(nums) - 1):
+            left = idx + 1
+            right = len(nums) - 1
+
+            while left < right:
+                if nums[idx] + nums[left] + nums[right] == 0:
+                    out.append((nums[idx], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+
+                elif nums[idx] + nums[left] + nums[right] < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return out
+        
+        # if len(nums) < 3: return False
+        # from itertools import permutations
+        # sol = dict.fromkeys(tuple(sorted(i)) for i in permutations(nums, 3) if sum(i) == 0)
+        # return sol.keys()
 
 
 def main():
