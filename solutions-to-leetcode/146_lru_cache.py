275d2fcbef9b8bc5f9e08f709db274a8525db7de
Kashif Memon <kashif.memon@risksense.com>
2019-03-29 12:00:16 -0600

    + leetcode

--- solutions-to-leetcode/146_lru_cache.py
+++ solutions-to-leetcode/146_lru_cache.py
@@ -5,43 +5,81 @@ https://leetcode.com/problems/lru-cache/
 _author:            Kashif Memon
 _python_version:    3.7.2
 """
-
 from collections import OrderedDict
 
 
-class LRUCache(OrderedDict):
+class Node(object):
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
+
+# This idea uses a dictionary with doubly LinkedList to work with O(1) complexity
+# dic stores {key, node}
+# Idea is; any new kv added, is added at start of LL. so oldest items (removable) is behind tail always
+class LRUCache(object):
     def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
         self.capacity = capacity
+        self.dic = {}  # [0]
+        self.head = Node(0, 0)  # [3]
+        self.tail = Node(0, 0)  # [3]
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def promote(self, node):  # [1]
+        # set the node next to head
+        temp = self.head.next
+        node.next = temp
+        temp.prev = node
+        self.head.next = node
+        node.prev = self.head
 
     def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        if key not in self:
-            return - 1
-
-        self.move_to_end(key)
-        print('g', self)
-        return self[key]
+        if key in self.dic:
+            node = self.dic[key]
+            self.remove(node)
+            self.promote(node)
+            return node.val
+        return -1
 
     def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
-        if key in self:
-            self.move_to_end(key)
-        self[key] = value
-        print('p1', self)
-        if len(self) > self.capacity:
-            self.popitem(last=False)
-            print('p2', self)
+        if key in self.dic:
+            self.remove(self.dic[key])
+        node = Node(key, value)
+        self.promote(node)
+        self.dic[key] = node
+
+        if len(self.dic) > self.capacity:  # [2]
+            del self.dic[self.tail.prev.key]
+            self.remove(self.tail.prev)
+
+
+# The idea here is to use an orderedDict where
+# new items are always added at end of list (as normally).
+# any fetch request would move items to end of list, making start of list removable for newer items if necessary
+
+class LRUCache_dict:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache: return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
 
 
 def main():
