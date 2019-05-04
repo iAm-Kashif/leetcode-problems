ea77fdce52b261478f884cd323f18f93657de560
Kashif Memon <iAmKashifMemon@gmail.com>
2019-05-04 17:14:16 -0600

    + square-interview

---
+++ solutions-to-leetcode/square-interview/127_word_ladder.py
@@ -0,0 +1,49 @@
+"""
+127. Word Ladder
+https://leetcode.com/problems/word-ladder/
+
+_author:            Kashif Memon
+_python_version:    3.7.2
+"""
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: 'List[str]') -> int:
+
+        if endWord not in wordList or not beginWord or not endWord or not wordList: return 0
+
+        L = len(beginWord)
+
+        # Preprocess the wordList into Dictionary with all possible combinations
+        import collections
+        wordDict = collections.defaultdict(list)
+
+        for word in wordList:
+            for idx in range(L):
+                wordDict[word[:idx] + "*" + word[idx + 1:]].append(word)
+
+        # wordLadder Traverse using Queue (BFS)
+        q = [(beginWord, 1)]
+        visited = {beginWord: True}
+
+        while q:
+            curr_word, level = q.pop(0)
+
+            for idx in range(L):
+                intermediate_word = curr_word[:idx] + "*" + curr_word[idx + 1:]
+
+                for word in wordDict[intermediate_word]:
+                    if word == endWord:
+                        return level + 1
+                    if word not in visited:
+                        visited[word] = True
+                        q.append((word, level + 1))
+        return 0
+
+
+def main():
+    print(Solution().ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+
+
+if __name__ == "__main__":
+    main()
