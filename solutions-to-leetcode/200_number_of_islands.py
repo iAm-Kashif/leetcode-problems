326aa6e65748630e760a1971394a4ee619e311de
Kashif Memon <iamkashifmemon@gmail.com>
2020-08-02 18:51:00 -0700

    August Challenge

--- solutions-to-leetcode/200_number_of_islands.py
+++ solutions-to-leetcode/200_number_of_islands.py
@@ -8,33 +8,27 @@ _python_version:    3.7.2
 
 
 class Solution:
-    def numIslands(self, grid) -> int:
-
-        if not len(grid):
+    def numIslands(self, grid):
+        if not grid:
             return 0
-
-        rows = len(grid)
-        columns = len(grid[0])
         count = 0
+        visited = ()
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
 
-        for i in range(rows):
-            for j in range(columns):
-                val = grid[i][j]
-                # print(">> Considering({},{}); Val: {}".format(i, j, val))
-
-                if val == 0:
-                    self.dfs(grid, i, j)
+                if grid[r][c] == "1":
+                    self.recurse(grid, r, c)
                     count += 1
         return count
 
-    def dfs(self, grid, i, j):
-        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != 0:
+    def recurse(self, grid, r, c):
+        if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != "1":
             return
-        grid[i][j] = "#"
-        self.dfs(grid, i + 1, j)
-        self.dfs(grid, i - 1, j)
-        self.dfs(grid, i, j + 1)
-        self.dfs(grid, i, j - 1)
+        grid[r][c] = "#"
+        self.recurse(grid, r + 1, c)
+        self.recurse(grid, r - 1, c)
+        self.recurse(grid, r, c + 1)
+        self.recurse(grid, r, c - 1)
 
 
 def main():
